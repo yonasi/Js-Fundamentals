42715fc792fc62b63feb8021a7be59751fa1a56e
Yonas Mamo <yonasma416@gmail.com>
2025-09-14 03:49:37 -0700

    typo corrected on output

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const args = process.argv.length;
 if (args === 2) {
   console.log("No argument");
 } else if (args === 3) {
-  console.log("1 argument");
-} else {
   console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
